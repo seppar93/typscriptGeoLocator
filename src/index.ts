5f99d82f9af838f1ca2184b609a0bb86802e5bcf
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-20 13:41:29 -0400

    setting cutomMap class

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-import { User } from './User'; // << when using just the export and not export default use curly
-import {Company} from './Company'
-const user = new User ();
-const company = new Company()
+// import { User } from './User'; // << when using just the export and not export default use curly
+// import {Company} from './Company'
+import { CustomMap } from "./CustomMap";
 
-console.log(user);
-console.log(company);
+new CustomMap('map')
 
- 
+
