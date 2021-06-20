f736f76c8123fd519214f3efc3adf6b4e217320f
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-19 23:09:09 -0400

    company & name setup

--- src/User.ts
+++ src/User.ts
@@ -1,3 +1,15 @@
-class User {
-    
+import faker, { fake } from 'faker'
+export class User {
+    name: string;
+    location: {
+        lat: number;
+        lng: number
+    }
+    constructor() {
+        this.name = faker.name.firstName();
+        this.location = {
+            lat: parseFloat(faker.address.latitude()),
+            lng: parseFloat(faker.address.longitude())
+        }
+    }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
-console.log('GIVE ME THAT GOOD SUCK');
+import { User } from './User'; // << when using just the export and not export default use curly
+import {Company} from './Company'
+const user = new User ();
+const company = new Company()
+
+console.log(user);
+console.log(company);
+
+ 
