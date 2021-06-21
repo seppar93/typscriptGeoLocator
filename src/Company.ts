36280ead760836173e7a394379dee2fdf8c71e64
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-21 07:33:02 -0400

    cleanup

--- src/Company.ts
+++ src/Company.ts
@@ -1,12 +1,14 @@
 import faker from 'faker'
+import {Mappable} from './CustomMap'
 
-export class Company {
+export class Company implements Mappable {
     companyName: string;
     catchPhrase: string;
     location: {
         lat: number;
         lng: number;
-    }
+    };
+    color: string = 'red';
     constructor (){
         this.companyName = faker.company.companyName();
         this.catchPhrase = faker.company.catchPhrase();
@@ -14,5 +16,11 @@ export class Company {
             lat: parseFloat(faker.address.latitude()), 
             lng: parseFloat(faker.address.longitude())
         }
+    }
+    markerContent():string {
+        return `<div>
+        <h3>Company Name: ${this.companyName}</h3>
+        <h6>CatchPhrase: ${this.catchPhrase}</h6>
+        </div>`
     }
 }
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,14 @@
-// import { User } from './User'; // << when using just the export and not export default use curly
-// import {Company} from './Company'
+import { User } from './User'; // << when using just the export and not export default use curly
+import {Company} from './Company'
 import { CustomMap } from "./CustomMap";
 
-new CustomMap('map')
+const user = new User();
+const company = new Company();
+
+const customMap = new CustomMap('map');
+
+customMap.addMarker(user)
+customMap.addMarker(company)
+
 
 
